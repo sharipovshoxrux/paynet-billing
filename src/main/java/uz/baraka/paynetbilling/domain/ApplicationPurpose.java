7e64d8fb2a2f5bab149cef8c17a0353cc8501e66
shohruhsharipov <s.ugli@socialtech.uz>
2025-11-12 10:40:08 +0500

    ISSUE_AND_PRINT added key into purpose

--- src/main/java/uz/baraka/paynetbilling/domain/ApplicationPurpose.java
+++ src/main/java/uz/baraka/paynetbilling/domain/ApplicationPurpose.java
@@ -3,7 +3,7 @@ package uz.baraka.paynetbilling.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum ApplicationPurpose {
-    ISSUE, RE_ISSUE;
+    ISSUE, RE_ISSUE, ISSUE_AND_PRINT;
 
     @JsonCreator
     public static ApplicationPurpose from(String v) {
